a7dc2eb005109892416f43414bb474d889400d5b
Camilo Enrique Rivera <camilo.rive007@gmail.com>
2016-12-09 12:45:42 -0500

    Update Assignment1.fs

--- COP4555/Assignment1.fs
+++ COP4555/Assignment1.fs
@@ -1,21 +1,47 @@
-//Question 1
-    let rec gcd = function
-        | (a,0) -> a
-        | (a,b) -> gcd (b, a % b)
+// Team Members: Camilo Rivera
+// Fernando Acosta
+// Question 1
+let rec gcd = function
+| (a,0) -> a
+| (a,b) -> gcd (b, a % b)
 
-    let rec lcm = function
-        | (a,0) -> a
-        | (a,b) -> ((abs a)/gcd(a,b))*(abs b)
+let simplify (a,b) =
+    let x = gcd (a,b)
+    (a/x, b/x)
+    
+let (.+) (a,b) (c,d) = simplify (a*d + c*b, b*d)
 
-    let simplify (a,b)= 
-        let x = gcd (a,b)
-        (a/x, b/x)
+let (.*) (a,b) (c,d) = simplify (a*c, b*d)
 
+// Question 2
+let revlists xs = List.map List.rev xs;;
 
-    let (.+) (a,b) (c,d)=
-        let x = lcm(b,d) 
-        simplify (a*(x/b) + c*(x/d),x)
+// Question 3
+let rec interleave (xs, ys) =
+    match (xs, ys) with
+    | (xs,[]) -> xs
+    | ([],ys) -> ys
+    | (x::xs, y::ys) -> x::y::interleave (xs, ys)
+    
+// Question 4
+let rec gencut (n, xs) =
+    match (n, xs) with
+    | (0, xs) -> ([], xs)
+    | (n, []) -> ([], [])
+    | (n, x::xs) ->
+        let gentemp = gencut(n-1, xs)
+        (x::fst gentemp, snd gentemp)
+        
+let cut xs = gencut ((List.length xs)/2, xs)
 
-    let (.*)  (a,b) (c,d)=
-        simplify (a*c,b*d)
-//Question 2
+// Question 5
+let shuffle xs = interleave (cut (xs))
+
+// Question 6
+let rec countaux (deck, target, n) =
+    let deck = shuffle deck
+    if deck = target then n else countaux(deck, target, n+1)
+    
+let countshuffles n =
+    let xs = [1..n]
+    countaux(xs, xs, 1)
